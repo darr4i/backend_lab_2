bed10d71854ff7f4d12870f81e29b91ce2b37043
darr4i <doshirrak999@gmail.com>
2024-10-30 23:08:23 +0200

    Add os import for PORT configuration

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from routes.routes import init_routes 
 from utils.utils import format_response, error_response  
+import os  # Додайте цей імпорт
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,4 +14,5 @@ def index():
     return {"message": "API is running!"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port) 
